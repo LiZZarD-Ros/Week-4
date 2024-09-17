90a30edfe238e55311dfb7156a30b40c23974dc8
LiZZarD-Ros <140954865+LiZZarD-Ros@users.noreply.github.com>
2024-09-17 21:27:13 +0200

    Day_2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    public bool hasPowerUp = false;
+
+    public float powerUpStrength = 15f;
+
+    public GameObject powerUpIndictor;
 
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +28,42 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput *  speed);
+        powerUpIndictor.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+
+        if (other.CompareTag("PowerUp"))
+        {
+           
+            hasPowerUp = true;
+            powerUpIndictor.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerUpIndictor.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+
+            
+            Debug.Log("Collided");
+        }
+    }
+
 }
